f1c599390c1149709db7cabf36b0582b4e1dccba
katerz avocado <ktrinaapodaca@yahoo.com>
2021-10-08 04:26:07 -0600

    updating

---
+++ client/build/precache-manifest.cafd416629e5baf0db6461a577b055ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf805d88210304bfcd674eb3b2601882",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dd264afc53404cadd82",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "586024da99e72c91c113",
+    "url": "/static/js/2.177c9537.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.177c9537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dd264afc53404cadd82",
+    "url": "/static/js/main.eedf0b8b.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
